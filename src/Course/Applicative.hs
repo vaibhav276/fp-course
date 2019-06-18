f1b671fc9ffc8a288a2969903174749fd52719f6
Vaibhav Pujari <276vaibhav+github@gmail.com>
2019-06-18 14:43:57 +0530

    Applicative Optional

--- src/Course/Applicative.hs
+++ src/Course/Applicative.hs
@@ -90,14 +90,13 @@ instance Applicative Optional where
   pure ::
     a
     -> Optional a
-  pure =
-    error "todo: Course.Applicative pure#instance Optional"
+  pure = Full
   (<*>) ::
     Optional (a -> b)
     -> Optional a
     -> Optional b
-  (<*>) =
-    error "todo: Course.Apply (<*>)#instance Optional"
+  Full f <*> Full a = Full (f a)
+  _      <*> _      = Empty
 
 -- | Insert into a constant function.
 --
