10353588cec60a21d7154c4d9aa2e0bccb0a1679
Vaibhav Pujari <276vaibhav+github@gmail.com>
2019-06-27 13:30:47 +0530

    Functor Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,8 +16,7 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) f (Compose fga) = Compose ((f <$>) <$> fga)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
