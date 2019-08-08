b3203d4f865cec396864538908256220ad4d8042
Vaibhav Pujari <276vaibhav+github@gmail.com>
2019-08-08 15:21:57 +0530

    ListZipper Comonad

--- src/Course/ListZipper.hs
+++ src/Course/ListZipper.hs
@@ -746,8 +746,8 @@ instance Extend MaybeListZipper where
 -- >>> copure (zipper [2,1] 3 [4,5])
 -- 3
 instance Comonad ListZipper where
-  copure =
-    error "todo: Course.ListZipper copure#instance ListZipper"
+  copure :: ListZipper a -> a
+  copure (ListZipper _ x _) = x
 
 -- | Implement the `Traversable` instance for `ListZipper`.
 -- This implementation traverses a zipper while running some `Applicative` effect through the zipper.
